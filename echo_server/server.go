5c25bc333f46caa365987b5ac6ef02c76eb635fc
zht <zhoutong.zht@gmail.com>
2019-08-17 00:39:12 +0800

    回声服务新增位移选项-step, 倒置字符串选项-r与多倍放大选项-m合并

--- echo_server/server.go
+++ echo_server/server.go
@@ -3,29 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net"
+	"sync/atomic"
 )
 
 var (
-	host     = flag.String("h", "0.0.0.0", "host ip")
-	port     = flag.Int("p", 12345, "port")
-	reverse  = flag.Bool("r", true, "reverse")
-	udp      = flag.Bool("u", false, "udp or tcp")
-	noprint  = flag.Bool("z", false, "no print")
-	multiple = flag.Int("m", 1, "multiple times echo back")
-	max_size = 1 << 20
+	host = flag.String("h", "0.0.0.0", "host ip")
+	port = flag.Int("p", 9999, "port")
 
-	limit  = flag.Int("l", 1024, "limit")
-	parall chan bool
+	udp      = flag.Bool("u", false, "udp or tcp")
+	print    = flag.Bool("v", false, "print")
+	multiple = flag.Int("m", 1, "multiple times echo back, negtive means to reverse")
+	step     = flag.Int("step", 0, "positive means shift left, negtive means shift right, when multiple is not -1/0/1")
+	// limit    = flag.Int("l", 1024, "limit")
+
+	max_size = 1024
+	reverse  bool
+	// parall   chan bool
+	parallCount int32
 )
 
 func main() {
 	flag.Parse()
-	parall = make(chan bool, *limit)
-	for i := 0; i < *limit; i++ {
-		parall <- true
+	if *multiple < 0 {
+		*multiple = -*multiple
+		reverse = true
 	}
+
+	// parall = make(chan bool, *limit)
+	// for i := 0; i < *limit; i++ {
+	// 	parall <- true
+	// }
 
 	if *udp {
 		listenUDP()
@@ -37,35 +45,50 @@ func main() {
 func listenUDP() {
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP(*host), Port: *port})
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 		return
 	}
 	fmt.Printf("listen at udp:%v:%v\n", *host, *port)
 
-	data := make([]byte, 1024)
+	data := make([]byte, max_size)
 	for {
 		n, remoteAddr, err := listener.ReadFromUDP(data)
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
 		}
 
-		if !*noprint {
-			log.Printf("<%s> %s\n", remoteAddr, data[:n])
+		// <-parall
+		go echoBack(data[:n], func(out []byte) (int, error) {
+			return listener.WriteToUDP(out, remoteAddr)
+		})
+
+		if *print {
+			fmt.Printf("<%s>%s(para: %v)\n", remoteAddr, data[:n], parallCount)
 		}
 
-		<-parall
-		go echoUDP(listener, remoteAddr, data, n)
 	}
 }
 
-func echoUDP(listener *net.UDPConn, remoteAddr *net.UDPAddr, data []byte, n int) {
-	var out_data = multipleData(reverseData(data[:n], *reverse))
-	_, err := listener.WriteToUDP(out_data, remoteAddr)
-	if err != nil {
-		log.Println(err)
+func echoBack(data []byte, f func([]byte) (int, error)) {
+	atomic.AddInt32(&parallCount, 1)
+	defer func() { atomic.AddInt32(&parallCount, -1) }()
+	var out_data = reverseData(data)
+	var distance = *step
+	if len(out_data) < 2 {
+		distance = 0
 	}
-
-	parall <- true
+	for distance < 0 {
+		distance += len(out_data)
+	}
+	for i := 0; i < *multiple; i++ {
+		out_data = append(out_data[distance:], out_data[:distance]...)
+		// fmt.Println(string(out_data))
+		_, err := f(out_data)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
 
 func listenTCP() {
@@ -92,42 +115,30 @@ func handle(conn net.Conn) {
 	defer conn.Close()
 
 	data := make([]byte, max_size)
-
 	for {
-		<-parall
+		// <-parall
 		n, err := conn.Read(data)
-		// if err == io.EOF || io.e {
-		// 	break
-		// } else
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
 			break
 		}
 
-		if !*noprint {
-			fmt.Printf("<%s>%s(parall: %v)\n", conn.RemoteAddr(), data[:n], *limit-len(parall))
+		if *print {
+			fmt.Printf("<%s>%s(parall: %v)\n", conn.RemoteAddr(), data[:n], parallCount)
 		}
 
-		go echoTCP(conn, data[:n])
+		go echoBack(data[:n], func(out []byte) (int, error) {
+			return conn.Write(out)
+		})
 	}
 }
 
-func echoTCP(conn net.Conn, data []byte) {
-	var out_data = multipleData(reverseData(data, *reverse))
-	_, err := conn.Write(out_data)
-	if err != nil {
-		log.Println(err)
-	}
-
-	parall <- true
-}
-
-func reverseData(data []byte, reverse bool) (out []byte) {
-	n := len(data)
+func reverseData(data []byte) (out []byte) {
 	if !reverse {
-		return data[:n]
+		return data
 	}
 
+	n := len(data)
 	out = make([]byte, n)
 	if data[n-1] == '\n' {
 		out[n-1] = '\n'
