7cae5e0c9ef0edee30cc6165caf63eb89797c7d2
zht <zhoutong.zht@gmail.com>
2019-12-13 00:45:05 +0800

    calc speed

--- transfer/main.go
+++ transfer/main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"math"
 	"net"
+	"sync/atomic"
+	"time"
 )
 
 var (
-	listen = flag.String("l", ":10556", "listen")
-	dst    = flag.String("d", "192.168.0.103:10556", "destination address")
+	listen = flag.String("l", ":19999", "listen")
+	dst    = flag.String("d", "127.0.0.1:9999", "destination address")
 	// src        = flag.String("s", "192.168.0.109:10555", "source address")
 	network    = flag.String("net", "udp", "network")
-	packetsize = flag.Int("size", 2048, "packet size")
+	packetsize = flag.Int("size", 10240, "packet size")
 	print      = flag.Bool("v", false, "print")
+	uint       = flag.String("uint", "k", "bits uint: m,k,b ")
+
+	bytesFromClient uint64
+	bytesFromServer uint64
+)
+
+const BITS = 8
+const (
+	B = 1 << (10 * iota)
+	K
+	M
+	G
 )
 
 func main() {
@@ -28,6 +44,8 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+
+	log.Printf("listen at %v, to %v\n", *listen, *dst)
 
 	// src_addr, err := net.ResolveUDPAddr(*network, *src)
 	// if err != nil {
@@ -45,6 +63,8 @@ func main() {
 				if *print {
 					log.Println("recv from dest", *dst, n)
 				}
+				atomic.AddUint64(&bytesFromServer, uint64(n))
+
 				m, werr := listen_conn.WriteTo(buf2[:n], src_addr)
 				if *print {
 					log.Println("send to src", src_addr, m)
@@ -62,16 +82,18 @@ func main() {
 		}
 	}()
 
+	go printSpeed()
 	buf := make([]byte, *packetsize)
 	var ltotal int
 	for {
 		var n int
 		var err error
 		n, src_addr, err = listen_conn.ReadFrom(buf)
-		if *print {
-			log.Println("recv from src", src_addr, n)
-		}
 		if n != 0 {
+			if *print {
+				log.Println("recv from src", src_addr, n)
+			}
+			atomic.AddUint64(&bytesFromClient, uint64(n))
 			m, werr := dst_conn.Write(buf[:n])
 			if *print {
 				log.Println("send to dst", *dst, m)
@@ -87,5 +109,48 @@ func main() {
 			continue
 		}
 	}
+}
 
+func printSpeed() {
+	ticker := time.NewTicker(time.Second)
+	var oldserver, oldclient uint64
+	for {
+		select {
+		case <-ticker.C:
+			serverCount, clientCount := atomic.LoadUint64(&bytesFromServer), atomic.LoadUint64(&bytesFromClient)
+			if clientCount == oldclient && serverCount == oldserver {
+				continue
+			}
+
+			tmp := clientCount
+			if clientCount < oldclient {
+				clientCount += (math.MaxUint64 - oldclient)
+			} else {
+				clientCount -= oldclient
+			}
+			oldclient = tmp
+
+			tmp = serverCount
+			if serverCount < oldserver {
+				serverCount += (math.MaxUint64 - oldserver)
+			} else {
+				serverCount -= oldserver
+			}
+			oldserver = tmp
+
+			base := B
+			danwei := ""
+			switch *uint {
+			case "k":
+				base = K
+				danwei = "K"
+			case "m":
+				base = M
+				danwei = "M"
+			}
+
+			sspeed, cspeed := float64(serverCount)/float64(base), float64(clientCount)/float64(base)
+			fmt.Printf("\r client %v speed: %9.2f %vbit/s, server %v speed: %9.2f %vbit/s         ", oldclient, cspeed*BITS, danwei, oldserver, sspeed*BITS, danwei)
+		}
+	}
 }
